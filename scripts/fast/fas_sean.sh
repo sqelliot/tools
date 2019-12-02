e02c28ffac690a3094459cf895605ecfc42eb84d
Elliott, Sean (US) <sean.elliott3@baesystems.com>
2019-12-02 17:27:52 +0000

    Add vars for different fast deployment types

--- scripts/fast/fas_sean.sh
+++ scripts/fast/fas_sean.sh
@@ -1,1 +1,7 @@
-cd ~/repos/fcms-deployment && ansible-playbook -vv plays/fas_one_button.yml -e build_no=-sean_elliott3-tester -e fas_version=99.99.T-SNAPSHOT -e deploy_types="fas-ml" -e make_asg=no -e deploy_zone=a -e '{"git": {"branch" : "dev"}}'
+#!/bin/bash
+
+source $HOME/repos/tools/bash/aws/.bashrc
+
+fas_apps="fas-ods-simulator,fas-sqs,fas-apps-track,fas-apps-customxml,fas-apps-nontrackstanag,fas-apps-nerd-ingest,fas-topic-broker,fas-track-init,fas-nerd-init"
+fas_inf="fas-kafka,fas-kafka-rest,fas-zookeeper,fas-ml"
+cd ~/repos/fcms-deployment && ap plays/fas_one_button.yml -e build_no=-sean_elliott3 -e fas_version=99.99.T-SNAPSHOT -e deploy_types=${fas_apps} -e make_asg=no -e deploy_zone=a 
