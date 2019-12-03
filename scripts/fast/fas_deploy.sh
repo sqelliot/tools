3a23ca5d5916d75b1ff6d7858827a70a8bdba2ef
Elliott, Sean (US) <sean.elliott3@baesystems.com>
2019-12-03 17:04:29 +0000

    Changes and file renaming fas_sean -> fas_deploy

---
+++ scripts/fast/fas_deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source $HOME/repos/tools/bash/aws/.bashrc
+
+fas_apps="fas-ods-simulator,fas-sqs,fas-apps-track,fas-apps-customxml,fas-apps-nontrackstanag,fas-apps-nerd-ingest,fas-topic-broker,fas-track-init,fas-nerd-init"
+fas_inf="fas-kafka,fas-kafka-rest,fas-zookeeper,fas-ml"
+fas_all="${fas_apps},${fas_inf}"
+
+fas_types=fas-ods-simulator,fas-sqs,fas-apps-track
+
+echo "Deploying: $fas_types"
+cd ~/repos/fcms-deployment && ap plays/fas_one_button.yml -e build_no=-sean_elliott3 -e fas_version=99.99.T-SNAPSHOT -e deploy_types=${fas_types} -e make_asg=no -e deploy_zone=a 
--- scripts/fast/fas_sean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source $HOME/repos/tools/bash/aws/.bashrc
-
-fas_apps="fas-ods-simulator,fas-sqs,fas-apps-track,fas-apps-customxml,fas-apps-nontrackstanag,fas-apps-nerd-ingest,fas-topic-broker,fas-track-init,fas-nerd-init"
-fas_inf="fas-kafka,fas-kafka-rest,fas-zookeeper,fas-ml"
-cd ~/repos/fcms-deployment && ap plays/fas_one_button.yml -e build_no=-sean_elliott3 -e fas_version=99.99.T-SNAPSHOT -e deploy_types=${fas_apps} -e make_asg=no -e deploy_zone=a 
